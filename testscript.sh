a188eeb2fa8b1acc13354ce68e5d0f3cfb94a645
adrianamarian <marianadriana01@yahoo.com>
2023-12-14 12:01:11 +0000

    adding somethin

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "!!dsadsai:!this script is from git"
+echo "!!dsadsai:!this script is from git_12/14/2023_0156"
 NUMBER=$((NUMBERONE + NUMBERTWO))
 echo "$NUMBER"
 echo "hope it works!!!!!!!!!cvdxcvdx!!!!!!"
